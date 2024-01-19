c4f170bf27a156f5a582834f382893fec3db7271
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2024-01-19 08:45:31 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,14 @@
 variable "resource_group_name" {
   type    = string
-  default = "foreach-resource-group"
 }
 
 variable "location" {
   type    = string
-  default = "uksouth"
 }
 
 variable "storage_account_name" {
   type    = set(string)
-  default = ["ramakrishna20240118", "prashanth20240118", "prasad20240118", "ram20240118", "suresh20240118"]
 }
 
 variable "tags" {
-  default = {
-    project     = "saanvikit"
-    environment = "dev"
-    owner       = "azureramakrishna@gmail.com"
-  }
 }
